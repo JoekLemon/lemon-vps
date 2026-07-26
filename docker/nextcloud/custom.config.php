7d1ce210eb238727e75a6dfc8c8a276974b3a2d8
JoekLemon <github.igxr4@passmail.com>
2026-07-26 10:58:00 +0100

    feat: add NextCloud custom config with Redis caching

---
+++ docker/nextcloud/custom.config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'overwriteprotocol' => 'https',
+  'overwrite.cli.url' => 'https://cloud.' . getenv('NEXTCLOUD_TRUSTED_DOMAINS'),
+  'trusted_proxies' => ['caddy'],
+
+  'default_phone_region' => 'US',
+
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.distributed' => '\\OC\\Memcache\\Memcached',
+
+  'filelocking.enabled' => true,
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
+
+  'redis' => array(
+    'host' => 'redis',
+    'port' => 6379,
+  ),
+
+  'maintenance_window_start' => 1,
+);
