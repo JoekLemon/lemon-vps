6deefbbbefce748b9a4ae43e5bc94b6001216809
JoekLemon <github.igxr4@passmail.com>
2026-07-29 22:28:58 +0100

    Add metadata header and change shebang to bash in ices/stream.sh

--- docker/ices/stream.sh
+++ docker/ices/stream.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+: '
+Title:          Icecast Stream Script
+Description:    Streams a music file to Icecast using FFmpeg.
+Author:         Joek Lemon
+Contributors:
+Notes:          Runs inside the ices container via Docker Compose.
+'
 exec ffmpeg -re -stream_loop -1 -i /music/music.mp3 -c copy -f mp3 \
   icecast://source:${ICECAST_SOURCE_PASS:-hackme}@icecast:8000/stream
